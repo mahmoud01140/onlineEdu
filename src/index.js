288327a368030639763ffe5dd35d944a4ec8b92f
mahmoud01140 <mmarouf373@gmail.com>
2025-10-24 21:37:28 +0300

    first upload fix an error

--- src/index.js
+++ src/index.js
@@ -14,26 +14,26 @@ const hpp = require("hpp");
 const cookieParser = require("cookie-parser");
 const compression = require("compression");
 const path = require("path");
-const app = express()
-// Middleware
+const app = express();
+app.set("trust proxy", 1);
 app.use(
   cors({
-    origin : process.env.CLIENT_URL || "http://localhost:3000",
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
     credentials: true,
   })
 );
-app.use(express.json({ limit: '5mb', extended: true }));
-app.use("/uploads", express.static("uploads"));
+app.use(express.json({ limit: "5mb", extended: true }));
+
 // db connection
 connectDB();
-const {AppError} = require("./middleware/error");
-const {globalErrorHandler} = require("./middleware/error");
+const { AppError } = require("./middleware/error");
+const { globalErrorHandler } = require("./middleware/error");
 const authRouter = require("./routes/authRoutes");
-const courseRoutes = require("./routes/courseRoutes")
-const userRoutes = require("./routes/userRoutes")
+const courseRoutes = require("./routes/courseRoutes");
+const userRoutes = require("./routes/userRoutes");
 const examRoutes = require("./routes/examRoutes");
-const groupRoutes = require("./routes/groupRoutes")
-const lessonRoutes = require("./routes/lessonRoutes")
+const groupRoutes = require("./routes/groupRoutes");
+const lessonRoutes = require("./routes/lessonRoutes");
 const studyRoutes = require("./routes/studyRoutes");
 const dashboardRoutes = require("./routes/adminStudyRoutes");
 const liveExamRoutes = require("./routes/liveExamRoutes");
@@ -42,7 +42,7 @@ const exportRoutes = require("./routes/exportRoutes");
 // 1) GLOBAL MIDDLEWARES
 // Enable CORS
 
-app.options('*', cors());
+app.options("*", cors());
 
 // Set security HTTP headers
 app.use(helmet());
@@ -98,19 +98,19 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-app.get("/testapp", (req, res) =>{
-  res.send("hello")
-})
+app.get("/testapp", (req, res) => {
+  res.send("hello");
+});
 app.use("/api/auth", authRouter);
 app.use("/api/courses", courseRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/exams", examRoutes);
 app.use("/api/groups", groupRoutes);
-app.use("/api/lessons", lessonRoutes)
+app.use("/api/lessons", lessonRoutes);
 app.use("/api/study", studyRoutes);
 app.use("/api/studyAdmin", dashboardRoutes);
 app.use("/api/liveExam", liveExamRoutes);
-app.use('/api/export', exportRoutes);
+app.use("/api/export", exportRoutes);
 // Handle unhandled routes
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
